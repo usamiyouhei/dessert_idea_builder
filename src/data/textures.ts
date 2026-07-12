1c6d1b084e1991f42b0f3ed09de840988acb870d
youhei usami <usao9110@icloud.com>
2026-07-12 09:50:25 +0400

    create data

---
+++ src/data/textures.ts
@@ -0,0 +1,20 @@
+import type { DessertOption } from "./dessertTypes";
+
+export const temperatures: DessertOption[] = [
+  {
+    value: "cold",
+    label: "冷たい",
+  },
+  {
+    value: "warm",
+    label: "温かい",
+  },
+  {
+    value: "hot",
+    label: "熱々",
+  },
+  {
+    value: "room-temperature",
+    label: "常温",
+  },
+];
