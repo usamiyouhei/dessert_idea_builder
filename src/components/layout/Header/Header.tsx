2673c9693247ed06686281983143866a33137ef4
youhei usami <usao9110@icloud.com>
2026-07-21 10:38:05 +0400

    update Header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,10 +1,31 @@
 import React from "react";
-import styles from "./Header.modules.scss";
+import styles from "./Header.module.scss";
 
-export default function Header() {
+type HeaderProps = {
+  title: string;
+  description?: string;
+  currentStep?: string;
+  totalSteps?: string;
+};
+
+export default function Header({
+  title,
+  description,
+  currentStep,
+  totalSteps,
+}: HeaderProps) {
   return (
-    <div>
-      <h1>header</h1>
-    </div>
+    <header className={styles.header}>
+      <div>
+        <h1 className={styles.title}>{title}</h1>
+        {description && <p className={styles.description}>{description}</p>}
+      </div>
+
+      {currentStep !== undefined && totalSteps !== undefined && (
+        <span>
+          Step {currentStep + 1} / {totalSteps}
+        </span>
+      )}
+    </header>
   );
 }
